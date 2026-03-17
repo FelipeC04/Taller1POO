f45082584cfef176cd3043b937bdd32a72a108c1
Felipe <juanf.cardonap@upb.edu.co>
2026-03-17 15:18:20 -0500

    Inicializar variables string con string.Empty en las clases Persona.cs y Materia.cs

--- SistemaNotas/Models/Materia.cs
+++ SistemaNotas/Models/Materia.cs
@@ -4,10 +4,10 @@ namespace SistemaNotas.Models;
 
 public class Materia : IIdentifiable
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public string Nombre { get; set; } = string.Empty;
     public int Creditos { get; set; }
 
     // AGREGACIÓN: Una materia 'tiene' un Profesor titular asignado, pero el profesor existe independientemente de la Materia (por eso ligamos relacion por el Id y no le damos ciclo de vida a la instancia interna).
-    public Guid? ProfesorTitularId { get; set; }
+    public int ProfesorTitularId { get; set; }
 }
--- SistemaNotas/Models/Persona.cs
+++ SistemaNotas/Models/Persona.cs
@@ -5,7 +5,7 @@ namespace SistemaNotas.Models;
 // HERENCIA - Clase abstracta base
 public abstract class Persona : IIdentifiable
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public string Nombres { get; set; } = string.Empty;
     public string Apellidos { get; set; } = string.Empty;
 }
